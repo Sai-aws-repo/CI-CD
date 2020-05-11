882f35864b588276dbb8009b0aa5deb4c27d402e
saikumard99 <51391890+saikumard99@users.noreply.github.com>
2020-05-10 19:08:35 -0500

    Update server.tf

--- Terraform/server.tf
+++ Terraform/server.tf
@@ -1,8 +1,7 @@
 resource "aws_instance" "pipeline_server"{
-    ami = data.aws_ami.pipeline_ami"
+    ami = "data.aws_ami.pipeline_ami"
     instance_type = "t2.micro"
     key_name = "Ansible.pem"
-    vpc_id = ""
     vpc_security_group_ids = [aws_security_group.pipeline_sg.id]
     subnet_id = "data"
     tags = {
